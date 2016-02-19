e8c8c8465df817eb4ad341c471efeb69735ba630
notapresent <notapresent@gmail.com>
2016-02-19 16:25:34 +0000

    TaskMaster code cleanup

--- ttupdater/taskmaster.py
+++ ttupdater/taskmaster.py
@@ -1,26 +1,22 @@
 from google.appengine.api.taskqueue import Queue, Task
 
-# from scraper import Scraper
-
 
 class TaskMaster(object):
     """Creates and enqueues tasks"""
-    def __init__(self, tq=None):
-        self.queue = tq or Queue()
+    def __init__(self):
+        self.queue = Queue()
 
     def add_feed_update_task(self):
         """Enqueue task updating feeds"""
         self.queue.add(Task(url='/tasks/update_feeds'))
 
     def add_torrent_task(self, torrent_entry):
-        """"Enqueue task for torrent entry represented by dict-like object"""
-        tt = Task(url='/tasks/torrent',params=torrent_entry)
-        self.queue.add(tt)
+        """"Enqueue task for torrent entry represented by parser.TorrentEntry named tuple"""
+        task = Task(url='/tasks/torrent',params=torrent_entry)
+        self.queue.add(task)
 
-    def add_new_torrents(self, scraper=None):
+    def add_new_torrents(self, scraper):
         """Enqueues tasks for all new torrents"""
-        scrp = scraper or Scraper()     # FIXME move this somewhere else
-
-        new_entries = scrp.get_new_torrents()
+        new_entries = scraper.get_new_torrents()
         for e in new_entries:
             self.add_torrent_task(e)
