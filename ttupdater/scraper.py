54ee68caec81a24fc6ae5f94636563405b510ac6
notapresent <notapresent@gmail.com>
2016-02-19 16:13:09 +0000

    Scraper code cleanup

--- ttupdater/scraper.py
+++ ttupdater/scraper.py
@@ -1,18 +1,21 @@
+from datetime import datetime
+
 from models import Torrent
+from webclient import WebClient
+from parser import Parser
 
 
 class Scraper(object):
     def __init__(self, webclient, parser):
         self.webclient = webclient
         self.parser = parser
-        self.dt_threshold = None
 
     """Extracts torrent data from tracker"""
     def get_new_torrents(self):
         """Returns list of torent entries for new torrents"""
         index_html = self.webclient.get_index_page()
-        entries = self.parser.parse_index(index_html)
-        return [e for entry in entries if self.is_new(e)]
+        all_entries = self.parser.parse_index(index_html)
+        return filter_new_entries(all_entries)
 
     def get_torrent_data(self, tid):
         """Returns data for torrent, specified by tid"""
@@ -20,8 +23,9 @@ class Scraper(object):
         entry = self.parser.parse_torrent_page(html)
         return entry
 
-    def is_new(self, entry):
-        """Returns true for new entries, obviously"""
-        if not self.dt_threshold:
-            self.dt_threshold = Torrent.get_latest_dt()     # TODO not sure where to put this
-        return entry.dt >= self.dt_threshold
+
+def filter_new_entries(entries):
+    """Returns only new entries from the list"""
+    dt_threshold = Torrent.get_latest_dt()     # XXX not sure where to put this
+    return [e for e in entries if datetime.utcfromtimestamp(entry.ts) >= dt_threshold]
+
