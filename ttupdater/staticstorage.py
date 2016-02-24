5fa0421e2c868bb481f1be879ae9ac2269a9ddc0
notapresent <notapresent@gmail.com>
2016-02-24 18:00:50 +0000

    GCS static storage implementation

--- ttupdater/staticstorage.py
+++ ttupdater/staticstorage.py
@@ -1,4 +1,6 @@
 """Static data storage"""
+import cloudstorage as gcs
+from google.appengine.api import app_identity
 
 
 class BaseStaticStorage(object):
@@ -10,3 +12,24 @@ class BaseStaticStorage(object):
     def url_for_path(self, path):
         """Get absolute url for object stored at path"""
         raise NotImplementedError()
+
+
+class GCSStorage(BaseStaticStorage):
+    """Google cloud storage backend"""
+    def __init__(self, bucket_name=None):
+        self.bucket_name = bucket_name or app_identity.get_default_gcs_bucket_name()
+
+    def make_full_path(self, path):
+        """Build full path from bucket name and given file path"""
+        return '/' + self.bucket_name + '/' + path.strip('/')
+
+    def put(self, path, content, content_type='text/html'):
+        fullname = self.make_full_path(path)
+        write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+        gcs_file = gcs.open(fullname, 'w', content_type=content_type,
+                            retry_params=write_retry_params)
+        gcs_file.write(content)
+        gcs_file.close()
+
+    def url_for_path(self, path):
+        return 'https://storage.googleapis.com/{}.appspot.com/{}'.format(self.bucket_name, path.strip('/'))
