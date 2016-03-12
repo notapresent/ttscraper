3d855c2b82d182ea7b646dea41da196b9450a155
notapresent <notapresent@gmail.com>
2016-03-12 19:43:51 +0000

    single-source package version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 from distutils.core import setup
 from os import path
 from codecs import open
@@ -9,13 +10,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open('ttscraper/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
 setup(name='ttscraper',
-    version='0.1',
+    version=version,
     description='Torrent tracker scraping library',
     long_description=long_description,
     author='notapresent@gmail.com',
     author_email='notapresent@gmail.com',
+    license='Apache 2.0',
     url='https://github.com/notapresent/ttscraper',
     packages=['ttscraper']
 )
